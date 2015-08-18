78af19b7408014f05a0e6e14f363dd6023c52d06
PraktikumVC <mens90@web.de>
2015-08-18 18:41:26 +0200

    namespace genutzt, code dadurch reduziert

--- ProjektX/ProjektX/Quelle1.cpp
+++ ProjektX/ProjektX/Quelle1.cpp
@@ -6,7 +6,6 @@
 #include <chrono>
 #include <ctime>
 
-#define USE249
 using namespace std;
 using namespace cv;
 
@@ -17,40 +16,40 @@ int main(int argc, char** argv)
 		<< CV_MINOR_VERSION << "."
 		<< CV_SUBMINOR_VERSION
 		<< std::endl;
-	cv::Mat im = cv::imread("adam1.png", 1);
+	Mat im = imread("adam1.png", 0);
 	if (im.empty())
 	{
 		cout << "Cannot open image!" << endl;
 		return -1;
 	}
-	cv::Mat gray;
-	cv::cvtColor(im, gray, cv::COLOR_BGR2GRAY);
+	Mat gray;
+	cvtColor(im, gray, cv::COLOR_BGR2GRAY);
 	int mnArea = 40 * 40;
 	int mxArea = im.rows*im.cols*0.4;
-	std::vector< std::vector< cv::Point > > ptblobs;
-	std::vector<cv::Rect> bboxes;
-	std::chrono::time_point<std::chrono::system_clock> start, end;
+	vector< vector< Point > > ptblobs;
+	vector<Rect> bboxes;
+	chrono::time_point<chrono::system_clock> start, end;
 
-	start = std::chrono::system_clock::now();
-#ifndef USE249
-	cv::Ptr<cv::MSER> mser = cv::MSER::create(1, mnArea, mxArea, 0.25, 0.2);
+	start = chrono::system_clock::now();
+
+	Ptr<cv::MSER> mser = cv::MSER::create(1, mnArea, mxArea, 0.25, 0.2);
 	mser->detectRegions(gray, ptblobs, bboxes);
-#else
 	
-	cv::MserFeatureDetector mser(1, mnArea, mxArea, 0.25, 0.2);
+	MserFeatureDetector mser(1, mnArea, mxArea, 0.25, 0.2);
 	mser(gray, ptblobs); 
-#endif
+
+	
 	end = std::chrono::system_clock::now();
 
-	std::chrono::duration<double> elapsed_seconds = end - start;
-	std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+	chrono::duration<double> elapsed_seconds = end - start;
+	time_t end_time = chrono::system_clock::to_time_t(end);
 
-	std::cout << "finished computation at " << std::ctime(&end_time)
+	cout << "finished computation at " << ctime(&end_time)
 		<< "elapsed time: " << elapsed_seconds.count() << "s\n";
 
-	cv::namedWindow("image", cv::WINDOW_NORMAL);
-	cv::imshow("image", im);
-	cv::waitKey(0);
+	namedWindow("image", cv::WINDOW_NORMAL);
+	imshow("image", im);
+	waitKey(0);
 
 	return 0;
 }
